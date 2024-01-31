201b6b527e1655659c9f1e4f5b7914f7fdbd8833
99omniaashraf <omniaashraf249@gmail.com>
2024-01-30 20:37:16 -0800

    Is BST

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -1,18 +1,22 @@
 #include "binary_trees.h"
 
 /**
- * isBST - compare node in order to check if a given.
+ * isBST - Compare node in order to check if a given
+ *
  * @tree: pointer to the root node of the tree to check
  * @min: min value
  * @max: max value
- * Return: 1 if success, 0 otherwise.
+ *
+ * Return: 1 if success, 0 otherwise
  */
 int isBST(const binary_tree_t *tree, int min, int max)
 {
 	if (tree == NULL)
 		return (1);
+
 	if (tree->n < min || tree->n > max)
 		return (0);
+
 	return (
 		isBST(tree->left, min, tree->n - 1) &&
 		isBST(tree->right, tree->n + 1, max)
@@ -20,14 +24,15 @@ int isBST(const binary_tree_t *tree, int min, int max)
 }
 
 /**
- * binary_tree_is_bst - checks if a binary tree is
- * a valid Binary Search Tree.
- * @tree: pointer to the root node of the tree to check.
- * Return: 1 if success, 0 otherwise.
+ * binary_tree_is_bst - checks if a binary tree is a valid Binary Search tree
+ *
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if success, 0 otherwise
  */
 int binary_tree_is_bst(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+
 	return (isBST(tree, INT_MIN, INT_MAX));
 }
