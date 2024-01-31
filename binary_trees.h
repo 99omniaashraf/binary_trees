34cea0e497692570d963dd738fd16f6db8aee44b
MariaRamzy <maria_botros94@hotmail.com>
2024-01-31 01:03:26 -0800

    is bst

--- binary_trees.h
+++ binary_trees.h
@@ -58,5 +58,9 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
+int binary_tree_is_bst(const binary_tree_t *tree);
+binary_tree_t *bta_helper(binary_tree_t *root, const binary_tree_t *first,
+			  const binary_tree_t *second);
+int btib_helper(const binary_tree_t *tree, int low, int hi);
 
 #endif /* _BINARY_TREES_H_ */
